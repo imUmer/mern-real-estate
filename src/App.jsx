c15c0e2c186326085581f77cbdd8fb18a201c27b
imUmer <imumerniaz@gmail.com>
2024-10-18 23:31:51 +0300

    created header and worked on it

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import {BrowserRouter, Routes , Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Signin from './pages/Signin'
+import Signout from './pages/Signout'
+import Header from './components/Header'
 export default function App() {
   return (
-    <h1 className="text-3xl text-red-500 font-bold ">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/signin' element={<Signin />} />
+        <Route path='/signout' element={<Signout />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import {FaSearch} from 'react-icons/fa';
+// import { FaSearch } from 'react-icons/fa';
+
+export default function Header() {
+  return (
+    <header className="bg-slate-300 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-2">
+        <h1 className="font-bold text-sm sm:text-xl flex flex-wrap ">
+          <span className="text-slate-400">Real</span>
+          <span className="text-slate-500">eState</span>
+        </h1>
+        <form className="bg-slate-100 p-2 rounded-md flex items-center ">
+            <input type="text" placeholder="Search..." className="bg-transparent focus:outline-none"  />
+             <FaSearch className="text-slate-600"/>
+        </form>
+      </div>
+    </header>
+  );
+}
